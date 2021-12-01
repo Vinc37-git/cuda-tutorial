b79f9d4a93498af3c137483c008265accf4f790f
vinc37-git <vinc37@web.de>
2021-12-01 11:59:46 +0000

    solved tut 2

---
+++ tutorial02/vector_add_grid.cu
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+#define N 100000000
+#define MAX_ERR 1e-6
+
+__global__ void vector_add(float *out, float *a, float *b, int n) {
+    // threadIdx.x contains the index of the thread within the block
+    // blockDim.x contains the size of thread block (number of threads in the thread block).
+
+    // blockIdx.x contains the index of the block with in the grid
+    // gridDim.x contains the size of the grid
+
+    int index = (blockIdx.x * blockDim.x) + threadIdx.x;  // linearisation of index tuple
+    int stride = gridDim.x * blockDim.x;  // 
+    for(int i = index /*"range" for every open thread*/; i < n; i += stride /* e.g + 256*/){
+        out[i] = a[i] + b[i];
+    }
+}
+
+int main(){
+    float *a, *b, *out;
+    float *d_a, *d_b, *d_out; 
+
+    // Allocate host memory
+    a   = (float*)malloc(sizeof(float) * N);
+    b   = (float*)malloc(sizeof(float) * N);
+    out = (float*)malloc(sizeof(float) * N);
+
+    // Initialize host arrays
+    for(int i = 0; i < N; i++){
+        a[i] = 1.0f;
+        b[i] = 2.0f;
+    }
+
+    // Allocate device memory
+    cudaMalloc((void**)&d_a, sizeof(float) * N);
+    cudaMalloc((void**)&d_b, sizeof(float) * N);
+    cudaMalloc((void**)&d_out, sizeof(float) * N);
+
+    // Transfer data from host to device memory
+    cudaMemcpy(d_a, a, sizeof(float) * N, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, sizeof(float) * N, cudaMemcpyHostToDevice);
+
+    // Executing kernel 
+    int threads = 1024;
+    int blocks = ((N + threads) / threads);
+    if (N / blocks > threads) {
+        printf("Error: block dimension too small"); 
+        return 0;
+    }
+    vector_add<<<blocks, threads>>>(d_out, d_a, d_b, N);
+    
+    // Transfer data back to host memory
+    cudaMemcpy(out, d_out, sizeof(float) * N, cudaMemcpyDeviceToHost);
+
+    // Verification
+    for(int i = 0; i < N; i++){
+        assert(fabs(out[i] - a[i] - b[i]) < MAX_ERR);
+    }
+
+    printf("PASSED\n");
+    printf("First: %f and last: %f element\n", out[0], out[N-1]);
+
+    // Deallocate device memory
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_out);
+    
+    // Deallocate host memory
+    free(a); 
+    free(b); 
+    free(out);
+
+}
